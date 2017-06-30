5baa5dad741310d8d09137ef0313dbf3f0eeda54
oracle <oracle@bigdatalite.localdomain>
2017-06-29 21:08:49 -0400

    PC Oracle 	crawlers/news_crawler/

--- crawlers/getVereadorData.py
+++ crawlers/getVereadorData.py
@@ -1,11 +1,12 @@
-import urllib3
+#import urllib3
+import urllib.request
 from Vereador import Vereador
 import re
 from bs4 import BeautifulSoup
 import json
 import datetime
 
-def dicionarioUrls(src_file="/home/oracle/TCC/crawlers/urls.txt"):
+def dicionarioUrls(src_file="urls.txt"):
 	urls=dict()
 	try:
 		fhand=open(src_file)
@@ -25,10 +26,11 @@ def getVereadores():
 	vereadoresL=list()
 	urlD=dicionarioUrls()
 	url=urlD['camara_vereadores']
-	http=urllib3.connectionpool.HTTPConnectionPool(url,maxsize=1)
+	print(url)
+	#http=urllib3.connectionpool.HTTPConnectionPool(url,maxsize=1)
 	print(hora_coleta,' Atualizando dados dos Vereadores. Utilizando a seguinte url: ',url+'/vereadores/')
-	resposta = http.request('GET','http://'+url+'/vereadores/')
-	pagina_vereadores=BeautifulSoup(resposta.data,"html.parser")
+	resposta = urllib.request.urlopen('http://'+url+'/vereadores/').read()
+	pagina_vereadores=BeautifulSoup(resposta,"html.parser")
 	lista_vereadores=pagina_vereadores.findAll('div',{'class':'vereadores-group'})
 	articles=lista_vereadores[0].findAll('article')
 	for vereador_data in articles:
@@ -41,6 +43,7 @@ def getVereadores():
 		vereador=vereador_data.find('p').find('a')
 		nome_vereador=str(vereador.text).strip()
 		pagina_vereador=vereador["href"]
+		pagina_url=("/"+pagina_vereador.split('/')[3]+"/"+pagina_vereador.split('/')[4])
 	#	#print(nome_vereador,' ',pagina_vereador)
 		v=Vereador(nome_vereador,sigla_partido,pagina_vereador)
 		vereadoresL.append(v)
@@ -56,9 +59,12 @@ def getVereadorDetail():
 	fhand=open('out.json','w')
 	for vereador in vereadores:
 		#coleta dos dados da pagina do vereador na camara - informacoes de contato e Biografia
-		http=urllib3.connectionpool.HTTPConnectionPool(url,maxsize=1)
-		resposta=http.request('GET',vereador.url_camara)
-		pagina_vereador=BeautifulSoup(resposta.data,"html.parser")
+		#http=urllib3.connectionpool.HTTPConnectionPool(url,maxsize=1)
+		x = vereador.url_camara
+		print(x)
+#		print(vereador.url_camara)
+		resposta=urllib.request.urlopen(x).read()
+		pagina_vereador=BeautifulSoup(resposta,"html.parser")
 		#print(pagina_vereador.prettify())
 		main=pagina_vereador.findAll('div',{'class':'cf','id':'main','role':'main'})
 		
