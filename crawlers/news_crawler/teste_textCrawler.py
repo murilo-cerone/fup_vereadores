0f9a53e525e0b53610e0cd61749eff9fc1e65b78
murilo-cerone <murilo-cerone@users.noreply.github.com>
2017-08-25 22:05:28 -0300

    progress!

--- crawlers/news_crawler/teste_textCrawler.py
+++ crawlers/news_crawler/teste_textCrawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 fhand=open('feeds.txt','r')
 
@@ -24,24 +25,40 @@ def getText(url):
 		print("erro ao fazer parsing ")
 		return None
 	
+def visible(element):
+	if element.parent.name in ['style','script','[document]','head','title']:
+		return False
+	elif re.match('<!--.*-->',str(element.encode('utf-8'))):
+		return False
+	else:
+		return True
+		
+	
 def getStrings(root):
-	tag=root.html
-	print(tag.attrs)
-	#return 
-	for s in tag.descendants:
-		if s.name=='script':
-			continue
-		else:
-			print(s.string)
-
-#		print ("valor de S: ",dir(s),type(s),s.name)
-		#if hasattr(s, 'name'):    # then it's a tag
-		#	if s.name == 'script':  # skip it!
-				#continue
-			#for x in getStrings(s): 
-				#yield x
-			#else:                     # it's a string!
-				#yield s
+	conteudos=list()
+	if root:
+		tag=root.body
+		data=tag.findAll(text=True)
+		resultado=filter(visible,data)
+		l = list(resultado)
+		for text in l:
+			if len(text.strip())>100:
+				print(text)
+				print('\n------------------------------------------------------------------\n')
+		
+		return
+		tags=tag.descendants
+		for t in tags:
+			x = input('')
+			print(t)
+	return 
 
 
 				
+urls=getUrls()
+i=0
+for url in urls:
+	getStrings(getText(url))
+	i=i+1
+	if i>2:
+		break
